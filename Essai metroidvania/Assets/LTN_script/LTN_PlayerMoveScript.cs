6aec025322c74d20e714980cbf4b5aba09ab2fc7
Luana-GD <lmtn2706@gmail.com>
2020-10-01 11:13:19 +0200

    Allez zou le projet dans le cul. Initial Push

---
+++ Essai metroidvania/Assets/LTN_script/LTN_PlayerMoveScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Threading;
+using UnityEngine;
+
+public class LTN_PlayerMoveScript : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+
+    public Rigidbody2D rb;
+    public Collider2D jumpVerify;
+    [SerializeField]
+    bool jump = false;
+
+    Vector2 movement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+}
